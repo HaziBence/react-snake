30aaea136e5fd6bf0c47904c9765dd0fee546a8b
Bence Házi <hazibence1109@gmail.com>
2020-05-24 22:31:51 +0200

    Updates

---
+++ precache-manifest.f3bd8724b7e67964ced2c99d641c4078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d128a2d5ee7c6dfb110a8a24cfa0c0d",
+    "url": "/react-snake/index.html"
+  },
+  {
+    "revision": "dc051e8d09b2e3556814",
+    "url": "/react-snake/static/css/main.8774d02f.chunk.css"
+  },
+  {
+    "revision": "4e71f7f0bc2975e26c19",
+    "url": "/react-snake/static/js/2.ce418849.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-snake/static/js/2.ce418849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc051e8d09b2e3556814",
+    "url": "/react-snake/static/js/main.e50b8abe.chunk.js"
+  },
+  {
+    "revision": "c2b04d00e98abd439472",
+    "url": "/react-snake/static/js/runtime-main.6b39d469.js"
+  }
+]);
